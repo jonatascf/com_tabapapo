9a3e64f6a57861977cfd6a7bd2c5f0b35c19a419
jonatascf <jonatas.ferreira@gmail.com>
2022-09-09 10:24:20 -0300

    Integration with Community Builder

--- com_tabapapo/admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `#__tabapapo`;
-DROP TABLE IF EXISTS `#__tabapapo_msg`;
-DROP TABLE IF EXISTS `#__tabapapo_usu`;
-
-DELETE FROM `#__content_types` WHERE type_alias in ('com_tabapapo.tabapapo','com_tabapapo.category');
